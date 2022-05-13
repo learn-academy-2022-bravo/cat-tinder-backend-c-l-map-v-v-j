8200f27e7c0668c46e8b21dc75af00e06e9a7594
ljkotalik <leannak14@gmail.com>
2022-05-13 14:15:58 -0700

    fixed matcher method and removed comments

--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -1,32 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Cat, type: :model do
-  it "must contain a name" do
-    cat = Cat.create age:4, enjoys:"Chewing on bags", image:"https://upload.wikimedia.org/wikipedia/commons/3/38/Adorable-animal-cat-20787.jpg"
-    p cat.errors[:name]
-    expect(cat.errors[:name]).to_not be_empty
+  describe "Create cat validations" do
+    it "must contain a name" do
+      cat = Cat.create age:4, enjoys:"Chewing on bags", image:"https://upload.wikimedia.org/wikipedia/commons/3/38/Adorable-animal-cat-20787.jpg"
+      expect(cat.errors[:name]).to_not be_empty
+    end
+    it "must contain an age" do
+      cat = Cat.create name:"Mr. Stark", enjoys:"Chewing on bags", image:"https://upload.wikimedia.org/wikipedia/commons/3/38/Adorable-animal-cat-20787.jpg"
+      expect(cat.errors[:age]).to_not be_empty
+    end
+    it "must contain an enjoys" do
+      cat = Cat.create name:"Mr. Stark", age:4, image:"https://upload.wikimedia.org/wikipedia/commons/3/38/Adorable-animal-cat-20787.jpg"
+      expect(cat.errors[:enjoys]).to_not be_empty
+    end
+    it "must contain an image" do
+      cat = Cat.create name:"Mr. Stark", age:4, enjoys:"Chewing on bags"
+      expect(cat.errors[:image]).to_not be_empty
+    end
+    it "enjoys must have a minimum length of 10" do
+      cat = Cat.create name:"Mr. Stark", age:4, enjoys:"Chewing on bags", image:"https://upload.wikimedia.org/wikipedia/commons/3/38/Adorable-animal-cat-20787.jpg"
+      expect(cat.errors[:enjoys]).to_not be_empty
+    end
   end
-  it "must contain an age" do
-    cat = Cat.create name:"Mr. Stark", enjoys:"Chewing on bags", image:"https://upload.wikimedia.org/wikipedia/commons/3/38/Adorable-animal-cat-20787.jpg"
-    p cat.errors[:age]
-    expect(cat.errors[:age]).to_not be_empty
-  end
-  it "must contain an enjoys" do
-    cat = Cat.create name:"Mr. Stark", age:4, image:"https://upload.wikimedia.org/wikipedia/commons/3/38/Adorable-animal-cat-20787.jpg"
-    p cat.errors[:enjoys]
-    expect(cat.errors[:enjoys]).to_not be_empty
-  end
-  it "must contain an image" do
-    cat = Cat.create name:"Mr. Stark", age:4, enjoys:"Chewing on bags"
-    p cat.errors[:image]
-    expect(cat.errors[:image]).to_not be_empty
-  end
-  it "enjoys must be at least 10 characters long" do
-    cat = Cat.create name:"Mr. Stark", age:4, enjoys:"Chewing on bags", image:"https://upload.wikimedia.org/wikipedia/commons/3/38/Adorable-animal-cat-20787.jpg"
-    p cat.errors[:enjoys]
-    expect(cat.errors[:enjoys]).to_not be_empty
-  end
-end
+end 
 
 
 
